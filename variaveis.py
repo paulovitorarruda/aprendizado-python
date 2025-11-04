378b76041f8b9a851261942f61f97904cdd95b18
paulovitorarruda <paulovitorparruda@gmail.com>
2025-11-04 12:57:53 -0300

    Aula 3
    Variáveis: são nomes que se referem a valores armazenados na memória e são criadas atribuindo um valor a um nome usando o sinal de igual (=).

---
+++ variaveis.py
@@ -0,0 +1,35 @@
+#Aula 3: Variáveis
+
+x = 5 # x recebe o valor 5
+y = 3.5
+z = 1+2j
+print(x)
+print(y)
+print(z) 
+
+w = "Paulo"
+w = 'Paulo'
+print(w)
+
+#A-z, 0-9 e _
+#altura, Altura, ALTURA
+
+h, j, l = 1, 3, 5
+print(h)
+print(j)
+print(l) 
+
+a = b = c = " faz curso de Python"
+print(a)
+print(b)
+print(c) 
+
+h = w + a
+print(h)
+
+l = x + j
+print(l)
+
+l = w + x
+print(l)
+# deu erro pois não tem como somar nome com número!
