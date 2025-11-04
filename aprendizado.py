0f1ba1cc2fec4411d73533f49bd587a12019e8e5
paulovitorarruda <paulovitorparruda@gmail.com>
2025-11-04 12:21:47 -0300

    Aula 1
    Fundamentos

---
+++ aprendizado.py
@@ -0,0 +1,53 @@
+#Aula 1: Fundamentos
+
+#algoritimo
+#localizar a impressora
+#verificar se a impressora está ligada
+
+"""conectar a impressora ao meu dispositivo
+abrir um arquivo em meu dispositivo
+solicitar a impressão"""
+
+print("Hello World!");
+
+
+The Zen of Python, por Tim Peters
+
+
+Bonito é melhor que feio.
+
+Explícito é melhor que implícito.
+
+Simples é melhor que complexo.
+
+Complexo é melhor que complicado.
+
+Plano é melhor que aglomerado.
+
+Esparso é melhor que denso.
+
+Legibilidade faz diferença.
+
+Casos especiais não são especiais o bastante para quebrar as regras.
+
+Embora a praticidade vença a pureza.
+
+Erros nunca devem passar silenciosamente.
+
+A menos que sejam explicitamente silenciados.
+
+Diante da ambiguidade, recuse a tentação de adivinhar.
+
+Deve haver um -- e preferencialmente só um -- modo óbvio para fazer algo.
+
+Embora esse modo possa não ser óbvio à primeira vista a menos que você seja holandês.
+
+Agora é melhor que nunca.
+
+Embora nunca frequentemente seja melhor que *exatamente* agora.
+
+Se a implementação é difícil de explicar, é uma má idéia.
+
+Se a implementação é fácil de explicar, pode ser uma boa idéia.
+
+Namespaces são uma grande idéia -- vamos fazer mais dessas!
