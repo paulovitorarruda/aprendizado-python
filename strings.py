1ef473be0948dd316166e48142dbd8fea6157d3d
paulovitorarruda <paulovitorparruda@gmail.com>
2025-11-12 17:28:07 -0300

    Aula 4
    Strings: é uma sequência de caracteres usada para armazenar texto, podendo incluir letras, números e símbolos, e é delimitada por aspas simples (') ou duplas (").

---
+++ strings.py
@@ -0,0 +1,27 @@
+print("Hello, World!")
+print('Hello, World!')
+
+w = "Aprendizado de Python3"
+print(w)
+
+"""comentário"""
+
+a = """Hello, World!
+Estou aprendendo Python"""
+print(a)
+
+#Métodos das Strings
+
+b = " remover espaços em branco "
+print(b.strip())
+
+c = "tudo fica minúsculos"
+print(c.lower())
+
+d = "tudo fica maiúsculos"
+print(d.upper())
+
+e = "Hello"
+f = "World"
+g = e + " " + f
+print(g)
