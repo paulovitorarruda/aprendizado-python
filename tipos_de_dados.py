7b42fdd0e4579db28acbe4071e78d0e61d49b4b3
paulovitorarruda <paulovitorparruda@gmail.com>
2025-11-12 21:56:06 -0300

    Aula 8
    Tipos de Dados: são agrupados em categorias como numéricos, sequenciais, de mapeamento, conjuntos e booleanos. Os tipos mais comuns incluem int (inteiro), float (ponto flutuante), str (texto), bool (booleano), list (lista), tuple (tupla) e dict (dicionário). Outros tipos importantes são complex (números complexos), bytes, bytearray e set (conjunto).

---
+++ tipos_de_dados.py
@@ -0,0 +1,20 @@
+"""
+Variáveis e ps Tipos de Dados
+
+Tipos de Dados:
+
+Tipos de Texto: str
+Tipos Númericos: int, float e o complex
+
+Tipos de Sequência: list, tuple, range
+Tipos de Mapeamento: dict
+Tipos Set: set, frozenset
+
+Tipo Booleano: bool
+Tipos Binários: bytes, bytearray, memoryview
+
+print()
+type()
+#casting float, int, str
+
+"""
