8620a5e6fd6ce570c3573ddbc29505f980d1dd37
paulovitorarruda <paulovitorparruda@gmail.com>
2025-11-12 21:22:57 -0300

    Aula 6
    Casting: é a conversão explícita do tipo de dado de uma variável usando funções como int(), float() e str().

---
+++ casting.py
@@ -0,0 +1,26 @@
+#int
+
+x = int(2)
+y = int(2.8)
+z = int('2')
+
+print(x, y, z)
+
+#float
+
+a = float(2.3)
+b = float(2)
+c = float('2.3')
+d = float('2')
+
+print(a, b, c, d)
+
+#str
+
+s = str('s1')
+t = str(2)
+r = str(2.3)
+
+print("A variável s é do tipo: ",type(s))
+print("A variável t é do tipo: ",type(t))
+print("A variável r é do tipo: ",type(r))
