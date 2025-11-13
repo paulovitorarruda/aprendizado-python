c1de1feaecadbaa482e0f5053333976759bfbc47
paulovitorarruda <paulovitorparruda@gmail.com>
2025-11-12 21:46:51 -0300

    Aula 7
    Escopo de Variáveis: refere-se a onde uma variável é acessível no código, com os principais tipos sendo local (dentro de funções) e global (fora de todas as funções).

---
+++ escopo_variaveis.py
@@ -0,0 +1,29 @@
+"""escopo de variáveis"""
+"""
+as variáveis globais
+
+as variáveis locais
+"""
+
+x = 5
+
+def funcao():
+  x = 3
+  print("Valor da vairável local:", x)
+
+funcao()
+print("Valor da vairável global:", x)
+
+funcao()
+
+n = 'Paulo' #nome
+a = 1.70 #altura
+c = '000.000.000-00' #cpf
+i = 21 #idade
+
+#forma mais rápida e simples
+
+nome = "Paulo"
+altura = 1.70
+cpf = "000.000.000-00"
+idade = 23
