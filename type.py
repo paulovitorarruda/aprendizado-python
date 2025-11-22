6fe933d668e846d6ae819809ad6af748e5e823cb
paulovitorarruda <paulovitorparruda@gmail.com>
2025-11-22 12:58:35 -0300

    Aula 9
    Instalando Extensões de Estilos: Flake8; Pylint; autopep8.
    E entendendo para que servem

--- type.py
+++ type.py
@@ -1,3 +1,5 @@
+"""Função Type"""
+
 a = "Hello, World!"
 b = 7
 c = 10.
@@ -7,3 +9,17 @@ print(type(a))
 print(type(b))
 print(type(c))
 print(type(d))
+
+# Flake8 é uma ferramenta de linha de comando para
+# Python que verifica o estilo e a qualidade do código.
+
+# Pylint é uma ferramenta de análise estática de código Python que verifica bugs,
+# erros de estilo e identifica possíveis problemas para melhorar a qualidade do código.
+
+# autopep8 é uma ferramenta de formatação de código Python que automaticamente
+# ajusta o código para seguir as diretrizes do guia de estilo PEP 8.
+
+x = 2
+
+if x == 3:
+    print("vamos")
