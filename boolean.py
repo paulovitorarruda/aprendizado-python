1baec9773d42d86db64edb5b85fa3c2225502cb4
paulovitorarruda <paulovitorparruda@gmail.com>
2026-01-24 13:00:59 -0300

    Aula 12
    Tipo Boolean: é um tipo lógico fundamental na computação que armazena apenas dois valores: verdadeiro (true) ou falso (false)

---
+++ boolean.py
@@ -0,0 +1,37 @@
+"""
+Operadores Lógicos e o Tipo Boolean
+
+Em ciência da computação, boolean, ou lógico,
+é um tipo de dado primitivo que possui dois valores,
+que podem ser considerados como (0 ou 1), falso ou verdadeiro.
+
+Chamado boolean em homenagem a George Boole
+
+"""
+
+# Tipo Boolean: True e False
+
+"""
+Operadores Lógicos
+
+and (e)
+or  (ou)
+not (negação)
+
+Tabela Verdade
+
+Utilizando o "E" (and)
+
+Verdadeiro e Verdadeiro - True and True = True
+Verdadeiro e Falso - True and False = False
+False e Verdadeiro - False and True = False
+Falso e Falso - False and False = False
+
+Utilizando o "OU" (or)
+
+Verdadeiro ou Verdadeiro - True or True = True
+Verdadeiro ou Falso - True or False = True
+False ou Verdadeiro - False or True = True
+Falso ou Falso - False or False = False
+
+"""
