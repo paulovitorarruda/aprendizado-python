434263dfc1d0ef905e05cf455e9c622e4afdb5c0
paulovitorarruda <paulovitorparruda@gmail.com>
2026-03-17 13:41:54 -0300

    Aula 14
    Operadores de Comparação: são utilizados para avaliar relações entre valores, retornando sempre um booleano: True (verdadeiro) ou False (falso). Os principais são == (igual), != (diferente), > (maior), < (menor), >= (maior ou igual) e <= (menor ou igual), essenciais para controle de fluxo em estruturas condicionais if/else.

---
+++ comparacao.py
@@ -0,0 +1,20 @@
+"""
+Operadores de Comparação
+
+Operador da Igualdade ==
+Operador da Diferença !=
+Operador maior que >
+Operador menor que <
+Operador maior que ou igual a >=
+Operador menor que ou igual a <=
+
+"""
+
+# Operador de atribuição =
+
+x = 3  # tipo int
+y = 5
+z = 3.0  # tipo float
+w = 8
+
+print(x == z)
