19149e7814ae307f6a29f8c395a498723dc6ac0d
paulovitorarruda <paulovitorparruda@gmail.com>
2026-03-29 11:25:44 -0300

    Aula 17
    Condicional com if, elif e else: As estruturas condicionais if, elif e else em Python permitem controlar o fluxo do programa, executando blocos de código com base em condições booleanas (verdadeiro ou falso). O if verifica a primeira condição, elif (else if) verifica condições adicionais, e else executa um bloco final se nenhum anterior for verdadeiro.

---
+++ .vscode/condicionais.py
@@ -0,0 +1,20 @@
+"""
+Python - Comandos de controle condicional
+
+if, else e alif -> (else if)
+
+"""
+x = 5
+y = 5
+
+if y > x:
+    print("y é maior do que x")
+elif y < x:
+    print("y é menor do que x")
+else:
+    print("y não é menor nem maior que x")
+
+print("Código fora do bloco condicional")
+print(y > x)
+print(y < x)
+print(y == x)
