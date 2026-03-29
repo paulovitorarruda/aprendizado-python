8caf3dcadf36f269d9141e2710ece650929ee788
paulovitorarruda <paulovitorparruda@gmail.com>
2026-03-29 12:01:03 -0300

    Aula 19
    Operador Ternário: é uma forma concisa de escrever estruturas condicionais if-else em uma única linha, utilizando três operandos: uma condição, um resultado para verdadeiro (?) e um para falso (:). Sua sintaxe é condição ? expr1 : expr2, retornando expr1 se a condição for true, ou expr2 se for false.

---
+++ .vscode/operador_ternario.py
@@ -0,0 +1,12 @@
+a = 8
+b = 5
+c = 3
+
+# if b > a: print("b é maior que a")
+# print("código fora do bloco if")
+
+# print("B") if b > a else print("A")
+
+if a > b:
+    if a > c:
+        print("A é maior que b e que c")
